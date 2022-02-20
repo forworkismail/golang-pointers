e72a2070e30cb9ccb6efdc1293b67c5b8e7304b3
Ismail <forwork.ismail@gmail.com>
2022-02-20 22:41:43 +0430

    Change the value of a pointer

--- pointers.go
+++ pointers.go
@@ -12,6 +12,16 @@ func main() {
 	// Pointer to the age variable
 	var myAge *int = &age
 	var myName *string = &name
+
+	// to get address of pointer
 	fmt.Println(myAge)
 	fmt.Println(myName)
+
+	// to get value of pointer
+	fmt.Println(*myAge)
+	fmt.Println(*myName)
+
+	// to change the value that a pointer is pointing to
+	*myAge = 33
+	fmt.Println(*myAge) // 33
 }
