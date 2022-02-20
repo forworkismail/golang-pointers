b08bb0641bf0f5d581b6d84bc948d95e2ba8f75d
Ismail <forwork.ismail@gmail.com>
2022-02-20 14:50:20 +0430

    Create pointer for string value

--- pointers.go
+++ pointers.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 	age := 32
+	name := "test"
 
 	fmt.Println(age)
+	fmt.Println(name)
 
 	// Pointer to the age variable
 	var myAge *int = &age
+	var myName *string = &name
 	fmt.Println(myAge)
+	fmt.Println(myName)
 }
